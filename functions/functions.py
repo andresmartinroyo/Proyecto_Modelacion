5736fa109fb6a1efb1e45fffe5322057fcb60e87
andresmartinroyo <andres.martin@correo.unimet.edu.ve>
2023-11-13 21:10:27 -0400

    Ya sirve Dijkstra!

--- functions/functions.py
+++ functions/functions.py
@@ -1,6 +1,7 @@
 import csv
 from Classes.Graph import Graph
 from Classes.Node import Node
+import heapq
 
 
 def read_csv(path):
@@ -21,11 +22,75 @@ def create_graph(path):
     edges_list = read_csv(path)
     for edge in edges_list:
         graph.add_edge(int(edge[0]),int(edge[1]),int(edge[2]))
+
+    """    
     graph.adjacency_matrix = graph.get_adjacency_matrix()
     graph.dijkstras_matrix = graph.get_dijkstras_matrix()
     for node in graph.adjacency_matrix:
         print(node)
     for node in graph.dijkstras_matrix:
         print(node)
+    """
         
     return graph
+
+def find_minimum(distances : list, visited : list):
+    minimum = distances[0]
+    index = 0
+    for i in range(0,len(visited)):
+        if visited[i] == 0 and minimum > distances[i]:
+            minimum = distances[i]
+            index = i
+    return index
+
+def find_closest_neighbors(neighbors : list):
+    minimum = neighbors[0][1]
+    index = 0
+    for i in range(0,len(neighbors)):
+        if minimum > neighbors[i][1]:
+            minimum = neighbors[i][1]
+            index = i
+    
+    return minimum, index
+
+def dijkstra(graph: Graph, source, destination):
+
+    # Inicialización
+    visited = set()
+    distances = {}
+    predecessors = []
+    for node in graph.nodes.keys():
+        distances[node] = float("inf")
+        predecessors.append(None)
+    distances[source] = 0
+
+    # Cola de prioridad
+    queue = []
+    heapq.heappush(queue, (distances[source], source))
+
+    # Algoritmo de Dijkstra
+    while queue:
+
+        # Obtener el nodo con la distancia más corta
+        current_distance, current_node = heapq.heappop(queue)
+
+        # Marcar el nodo como visitado
+        visited.add(current_node)
+
+        # Actualizar las distancias de los nodos adyacentes
+        for neighbor in graph.edges.get(current_node):
+            if neighbor[0] not in visited:
+                new_distance = current_distance + neighbor[1]
+                if new_distance < distances[neighbor[0]]:
+                    distances[neighbor[0]] = new_distance
+                    predecessors[neighbor[0]] = current_node
+                    heapq.heappush(queue, (new_distance, neighbor[0]))
+
+    # Retornar el camino y el costo
+    path = []
+    current_node = destination
+    while current_node != None:
+        path.append(current_node)
+        current_node = predecessors[current_node]
+    path.reverse()
+    return path, distances[destination]
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from functions.functions import *
 def main():
     javier_graph = create_graph("bd/javier_adj_list.csv")
     andreina_graph = create_graph("bd/andreina_adj_list.csv")
-    print("Hello, world")
+    path, costo = dijkstra(javier_graph, 7,31)
+    print(path)
+
 
 if __name__ == '__main__':
     main()
